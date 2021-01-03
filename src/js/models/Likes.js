a5a4d655ba9dca95797ac67192ea291ff416d745
Jesuloba <jesulobajohn@gmail.com>
2021-01-03 23:17:32 +0100

    clean coding

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -1,41 +1,41 @@
 export default class Likes {
-    constructor() {
-        this.likes = [];
-    }
+	constructor() {
+		this.likes = [];
+	}
 
-    addLike(id, title, author, img) {
-        const like = { id, title, author, img };
-        this.likes.push(like);
+	addLike(id, title, author, img) {
+		const like = { id, title, author, img };
+		this.likes.push(like);
 
-        // persist data in local storage
-        this.persistData();
+		// persist data in local storage
+		this.persistData();
 
-        return like;
-    }
+		return like;
+	}
 
-    deleteLike(id) {
-        const index = this.likes.findIndex(el => el.id === id);
-        this.likes.splice(index, 1);
+	deleteLike(id) {
+		const index = this.likes.findIndex((el) => el.id === id);
+		this.likes.splice(index, 1);
 
-        // persist data in local storage
-        this.persistData(); 
-    }
+		// persist data in local storage
+		this.persistData();
+	}
 
-    isLiked(id) {
-        return this.likes.findIndex(el => el.id === id) !== -1;
-    }
+	isLiked(id) {
+		return this.likes.findIndex((el) => el.id === id) !== -1;
+	}
 
-    getNumLikes() {
-        return this.likes.length;
-    }
+	getNumLikes() {
+		return this.likes.length;
+	}
 
-    persistData() {
-        localStorage.setItem('likes', JSON.stringify(this.likes));
-    }
+	persistData() {
+		localStorage.setItem("likes", JSON.stringify(this.likes));
+	}
 
-    readStorage() {
-        const storage = JSON.parse(localStorage.getItem('likes'));
+	readStorage() {
+		const storage = JSON.parse(localStorage.getItem("likes"));
 
-        if (storage) this.likes = storage;
-    }
+		if (storage) this.likes = storage;
+	}
 }
